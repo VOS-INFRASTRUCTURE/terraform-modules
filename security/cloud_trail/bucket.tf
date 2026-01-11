13461169b7f5d50a7154050830a9b9c554ad887d
debian <ibukunoreofe@gmail.com>
2026-01-11 23:33:48 +0100

    Implement conditional S3 bucket versioning: update versioning status to always enable when versioning is activated, enhancing configuration flexibility.

--- security/cloud_trail/bucket.tf
+++ security/cloud_trail/bucket.tf
@@ -70,10 +70,11 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "cloudtrail" {
 ################################################################################
 
 resource "aws_s3_bucket_versioning" "cloudtrail" {
+  count  = var.enable_bucket_versioning ? 1 : 0
   bucket = aws_s3_bucket.cloudtrail_logs.id
 
   versioning_configuration {
-    status = var.enable_bucket_versioning ? "Enabled" : "Suspended"
+    status = "Enabled"
   }
 }
 
