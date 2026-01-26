54d1e1396842afc6e2b603f93212acad91c601fa
debian <ibukunoreofe@gmail.com>
2026-01-26 23:29:38 +0100

    Add validation for log_retention_days variable in variables.tf

--- databases/ec2_mysql_arm/variables.tf
+++ databases/ec2_mysql_arm/variables.tf
@@ -167,8 +167,15 @@ variable "enable_cloudwatch_monitoring" {
 
 variable "log_retention_days" {
   description = "Number of days to retain CloudWatch logs"
+  # Must contain one of these values
+  # [1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 3653]
   type        = number
   default     = 7
+  # add validation
+  validation {
+    condition     = contains([1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 3653], var.log_retention_days)
+    error_message = "log_retention_days must be one of the following values: [1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, 3653]"
+  }
 }
 
 ################################################################################
