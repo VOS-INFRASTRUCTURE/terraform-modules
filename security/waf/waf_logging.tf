c738490faae53ce1bfbc8173d6b3560b18bc2341
debian <ibukunoreofe@gmail.com>
2026-02-14 08:20:16 +0100

    Fix WAF logging resource ARN to support multiple Web ACLs

--- security/waf/waf_logging.tf
+++ security/waf/waf_logging.tf
@@ -16,7 +16,7 @@ resource "aws_wafv2_web_acl_logging_configuration" "waf_logging" {
     aws_kinesis_firehose_delivery_stream.waf_logs
   ]
 
-  resource_arn = aws_wafv2_web_acl.waf.arn
+  resource_arn = aws_wafv2_web_acl.waf[0].arn
 
   log_destination_configs = [
     aws_kinesis_firehose_delivery_stream.waf_logs[0].arn
