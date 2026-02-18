bed32bff1104f5933a8f4f229e9d936b98539124
debian <ibukunoreofe@gmail.com>
2026-02-18 03:26:03 +0100

    Rename CloudWatch log group resource for EC2 x86 instance monitoring

--- ec2_servers/ec2_x86_docker/log_group.tf
+++ ec2_servers/ec2_x86_docker/log_group.tf
@@ -3,7 +3,7 @@
 # CloudWatch Log Group
 ################################################################################
 
-resource "aws_cloudwatch_log_group" "mysql_logs" {
+resource "aws_cloudwatch_log_group" "ec2_x86_logs" {
   count             = var.enable_cloudwatch_monitoring ? 1 : 0
   name              = "/aws/ec2/${local.instance_name}"
   retention_in_days = var.log_retention_days
